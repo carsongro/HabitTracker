e12ae88f8c458535263a2373d7d2f82d47ae7447
Carson <carsongro@gmail.com>
2022-06-12 14:58:56 -0600

    Initial Commit

---
+++ HabitTracker/HabitTrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  HabitTrackerApp.swift
+//  HabitTracker
+//
+//  Created by Carson Gross on 6/12/22.
+//
+
+import SwiftUI
+
+@main
+struct HabitTrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
